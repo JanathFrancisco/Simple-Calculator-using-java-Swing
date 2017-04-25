49250f3462f57835c10bc24ce8ba84c3e7522e55
Janath Francisco <Janath Francisco>
2017-04-25 07:06:11 +0530

    initial

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+ 
+package simplecalculator;
+
+  class SimpleCalculator {
+
+    
+    public static void main(String[] args) {
+      Calculator c = new Calculator();
+      c.setVisible(true);
+    }
+}
